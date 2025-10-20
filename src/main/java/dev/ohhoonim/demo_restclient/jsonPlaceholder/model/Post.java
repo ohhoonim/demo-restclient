aad1dc0dff3b28f434d51e69064a80116ea2e903
mathew <donnj95@gmail.com>
2025-10-20 09:24:17 +0900

    전체 도메인 모델

--- src/main/java/dev/ohhoonim/demo_restclient/jsonPlaceholder/model/Post.java
+++ src/main/java/dev/ohhoonim/demo_restclient/jsonPlaceholder/model/Post.java
@@ -66,3 +66,91 @@ rectangle "Post Management System" {
 @enduml
 
  */
+
+
+ /*
+@startuml
+skinparam ClassAttributeIconSize 0
+skinparam monochrome reverse
+
+' ----------------------------------------------------
+' Aggregate Root 정의 및 애그리거트 경계 표시
+' ----------------------------------------------------
+
+package "Post Aggregate" #DDDDDD {
+  class Post <<Aggregate Root>> {
+    + id : int
+    + userId : int <<ID Reference>>
+    --
+    + title : string
+    + body : string
+    --
+    + addComment(comment: Comment)
+  }
+
+  class Comment <<Entity>> {
+    + id : int
+    + postId : int <<FK>>
+    --
+    + name : string
+    + email : string
+    + body : string
+  }
+  ' Post와 Comment는 합성(Composition, 소유) 관계
+  Post "1" *-- "0..*" Comment : Comments
+}
+
+' ----------------------------------------------------
+' 외부 Aggregate Root 및 엔티티 정의
+' ----------------------------------------------------
+
+class User <<Aggregate Root>> {
+  + id : int
+  --
+  + name : string
+  + username : string
+  + email : string
+}
+
+package "Album Aggregate" #DDDDDD {
+  class Album <<Aggregate Root>> {
+    + id : int
+    + userId : int <<ID Reference>>
+    --
+    + title : string
+  }
+
+  class Photo <<Entity>> {
+    + id : int
+    + albumId : int <<FK>>
+    --
+    + title : string
+    + url : string
+  }
+  Album "1" *-- "0..*" Photo : Photos
+}
+
+class Todo <<Aggregate Root>> {
+  + id : int
+  + userId : int <<ID Reference>>
+  --
+  + title : string
+  + completed : boolean
+}
+
+
+' ----------------------------------------------------
+' 애그리거트 간의 ID 참조 관계 (느슨한 결합)
+' ----------------------------------------------------
+
+' Post는 User의 ID를 참조함
+Post .right.> User : userId 참조 (N:1)
+
+' Album은 User의 ID를 참조함
+Album .right.> User : userId 참조 (N:1)
+
+' Todo는 User의 ID를 참조함
+Todo .up.> User : userId 참조 (N:1)
+
+@enduml
+  */
